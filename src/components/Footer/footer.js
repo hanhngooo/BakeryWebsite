861261fa6197d6f87037a9cc2a839f423c53f595
hanhngooo <hanhngo1795@gmail.com>
2020-08-18 11:46:39 +0200

    Add animation for phone icon

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -12,24 +12,31 @@ function Footer() {
       </div>
       <div className="footer__contact">
         <div className="footer__contact--item">
+          <div className="footer__contact--item--phone">
+            <p className="footer__contact--paragraph">Liên hệ đặt bánh qua: </p>
+            <p className="footer__contact--paragraph">
+              <span className="footer__contact--phoneicon">
+                <FaPhoneAlt />
+              </span>{" "}
+              0778770284 (Chị Trang){" "}
+            </p>
+            <p className="footer__contact--paragraph">
+              <span className="footer__contact--phoneicon">
+                <FaPhoneAlt />{" "}
+              </span>
+              0907169569 (chị Trân){" "}
+            </p>
+
+            <div className="footer__contact--number"></div>
+          </div>
+        </div>
+        <div className="footer__contact--item">
           <a
             className="footer__contact--icon"
             href="https://www.facebook.com/profile.php?id=100008428893615"
           >
             <GrFacebook />
           </a>
-        </div>
-        <div className="footer__contact--item">
-          <div className="footer__contact--item--phone">
-            {/* <div className="footer__contact--icon"> */}
-
-            <p className="footer__contact--paragraph">
-              <FaPhoneAlt />{" "}
-              <span>0778770284 (Chị Trang) - 0907169569 (chị Trân)</span>
-            </p>
-            {/* </div> */}
-            <div className="footer__contact--number"></div>
-          </div>
         </div>
       </div>
       <div className="footer__copyright">
